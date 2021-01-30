08e4bbd8c3325dcefed66548e60926eee26cf7f4
mamesaburo <cinnabar.b.m.b.9597@gmail.com>
2021-01-30 13:13:43 +0900

    Coinlockerモデルを作成

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -3,4 +3,5 @@ class Platform < ApplicationRecord
   belongs_to_active_hash :elevator
   belongs_to_active_hash :escalator
   belongs_to_active_hash :toilet
+  belongs_to_active_hash :coinlocker
 end
---
+++ test/models/coinlocker_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CoinlockerTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

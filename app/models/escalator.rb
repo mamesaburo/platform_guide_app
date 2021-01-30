b67ee6341c670d7e118f3b21c7facdc605b9a03c
mamesaburo <cinnabar.b.m.b.9597@gmail.com>
2021-01-30 13:07:00 +0900

    Escalatorモデルを作成

---
+++ app/models/escalator.rb
@@ -0,0 +1,15 @@
+class Escalator < ApplicationRecord
+  self.data = [
+    { id: 0, name: '---' },
+    { id: 1, name: '進行方向 前' },
+    { id: 2, name: '中央' },
+    { id: 3, name: '進行方向 後' },
+    { id: 4, name: '進行方向 前・中央' },
+    { id: 5, name: '進行方向 前・進行方向 後' },
+    { id: 6, name: '中央・進行方向 後' },
+    { id: 7, name: '進行方向 前・中央・進行方向 後' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :platforms
+end
--- app/models/platform.rb
+++ app/models/platform.rb
@@ -1,4 +1,5 @@
 class Platform < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :elevator
+  belongs_to_active_hash :escalator
 end

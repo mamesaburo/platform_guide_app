5c9d65e013d5890f34de6bb69be10f75a63ec433
mamesaburo <cinnabar.b.m.b.9597@gmail.com>
2021-02-04 21:42:57 +0900

    Viewの表示・配置等の調節

--- app/models/escalator.rb
+++ app/models/escalator.rb
@@ -1,13 +1,13 @@
 class Escalator < ActiveHash::Base
   self.data = [
     { id: 0, name: '---' },
-    { id: 1, name: '進行方向 前' },
+    { id: 1, name: '前方' },
     { id: 2, name: '中央' },
-    { id: 3, name: '進行方向 後' },
-    { id: 4, name: '進行方向 前・中央' },
-    { id: 5, name: '進行方向 前・進行方向 後' },
-    { id: 6, name: '中央・進行方向 後' },
-    { id: 7, name: '進行方向 前・中央・進行方向 後' }
+    { id: 3, name: '後方' },
+    { id: 4, name: '前方・中央' },
+    { id: 5, name: '前方・後方' },
+    { id: 6, name: '中央・後方' },
+    { id: 7, name: '前方・中央・後方' }
   ]
 
   include ActiveHash::Associations

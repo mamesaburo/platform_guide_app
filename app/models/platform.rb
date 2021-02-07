6e2ae6a079dd312902efbd682e58ceade5d3795b
mamesaburo <cinnabar.b.m.b.9597@gmail.com>
2021-02-07 17:11:02 +0900

    バリデーションを追加

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -5,4 +5,12 @@ class Platform < ApplicationRecord
   belongs_to_active_hash :toilet
   belongs_to_active_hash :coinlocker
   belongs_to :station
+
+  with_options presence: true do
+    validates :number
+    validates :elevator_id
+    validates :escalator_id
+    validates :toilet_id
+    validates :coinlocker_id
+  end
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,4 +1,9 @@
 class Station < ApplicationRecord
   has_many :platforms
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+  end
 end
